790fa67a3608d82faadf382e5e88e5de6a83b9f9
gibbloggen <gibbloggen@gmail.com>
2016-03-16 22:03:26 -0400

    This is just the beginning of the layout and the pages.

--- DiabetesSolutions/MainPage.xaml.cs
+++ DiabetesSolutions/MainPage.xaml.cs
@@ -26,5 +26,53 @@ namespace DiabetesSolutions
         {
             this.InitializeComponent();
         }
+
+        private void ListBox_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (NewDay.IsSelected)
+            {
+
+                ContentFrame.Navigate(typeof(NewDay));
+                MenuSplitView.IsPaneOpen = false;
+
+
+            }
+            else if (thirty_Day.IsSelected)
+            {
+                ContentFrame.Navigate(typeof(_30Day));
+                MenuSplitView.IsPaneOpen = false;
+
+            }
+            else if (Ninety_Day.IsSelected)
+            {
+                ContentFrame.Navigate(typeof(_90Day));
+                MenuSplitView.IsPaneOpen = false;
+
+            }
+            else if (DoctorReports.IsSelected)
+            {
+                ContentFrame.Navigate(typeof(DoctorsReport));
+                MenuSplitView.IsPaneOpen = false;
+
+            }
+            else if (ImportReadings.IsSelected)
+            {
+                ContentFrame.Navigate(typeof(ImportReadings));
+                MenuSplitView.IsPaneOpen = false;
+
+            }
+            else if (ExportReadings.IsSelected)
+            {
+                ContentFrame.Navigate(typeof(ExportReadings));
+                MenuSplitView.IsPaneOpen = false;
+
+            }
+        }
+
+        private void MainMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            MenuSplitView.IsPaneOpen = !MenuSplitView.IsPaneOpen;
+
+        }
     }
 }
